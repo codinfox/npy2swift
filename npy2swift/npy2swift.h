dbf97b1da5e08cb74c8a291da5370d79efe1e71c
Zhihao <lzh727@126.com>
2016-04-30 01:33:41 -0400

    Initial Commit

---
+++ npy2swift/npy2swift.h
@@ -0,0 +1,19 @@
+//
+//  npy2swift.h
+//  npy2swift
+//
+//  Created by Zhihao Li on 4/30/16.
+//  Copyright © 2016 CMU. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for npy2swift.
+FOUNDATION_EXPORT double npy2swiftVersionNumber;
+
+//! Project version string for npy2swift.
+FOUNDATION_EXPORT const unsigned char npy2swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <npy2swift/PublicHeader.h>
+
+
